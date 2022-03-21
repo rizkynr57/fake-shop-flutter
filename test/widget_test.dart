cece9d44e82b9dcece88ada5bac987d9c248cf2b
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-21 16:40:11 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:parsing/main.dart';
+import 'package:fake-shop/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
